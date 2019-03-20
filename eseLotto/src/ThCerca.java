a306e205677f3ce19ba7a9f8acb8f564bcdd0e35
jack96489 <giacomo.orsenigo@gmail.com>
2019-03-20 15:31:39 +0100

    Thread cerca

--- eseLotto/src/ThCerca.java
+++ eseLotto/src/ThCerca.java
@@ -1,2 +1,38 @@
-public class ThCerca {
+import java.util.Random;
+
+public class ThCerca extends Thread {
+    private final DatiCondivisi ptrDati;
+    private final int numThread;
+
+    public ThCerca(DatiCondivisi ptrDati, int numThread) {
+        this.ptrDati = ptrDati;
+        this.numThread = numThread;
+    }
+
+    @Override
+    public void run() {
+        Random rn = new Random();
+        final int daCercare;
+        if (numThread == 1)
+            daCercare = ptrDati.getPunatatoUno();
+        else
+            daCercare = ptrDati.getPuntatoDue();
+        try {
+            for (Ruota r : ptrDati.getRuote()) {
+                if (numThread == 1)
+                    ptrDati.getEstrattoUno().acquire();
+                else
+                    ptrDati.getEstrattoDue().acquire();
+                for (int i = 0; i < Ruota.getNumeriPerRuota(); i++)
+                    if (daCercare == r.getAt(i))
+                        if (numThread == 1)
+                            r.setVintoUno(true);
+                        else
+                            r.setVintoDue(true);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
