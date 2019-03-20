de1990af269e0804a14e9e847a4aef4f5a9cf44e
jack96489 <giacomo.orsenigo@gmail.com>
2019-03-20 15:22:35 +0100

    Thread estrai

--- eseLotto/src/DatiCondivisi.java
+++ eseLotto/src/DatiCondivisi.java
@@ -1,2 +1,40 @@
+import java.util.Vector;
+import java.util.concurrent.Semaphore;
+
 public class DatiCondivisi {
+    private Vector<Ruota> ruote;
+    private final int numRuote;
+    private final Semaphore estrattoUno, estrattoDue;
+    private final int punatatoUno, puntatoDue;
+
+    public DatiCondivisi(int numRuote, int punatatoUno, int puntatoDue) {
+        this.numRuote = numRuote;
+        this.punatatoUno = punatatoUno;
+        this.puntatoDue = puntatoDue;
+        ruote = new Vector<>();
+        for (int i =0;i<numRuote;i++)
+            ruote.add(new Ruota(i));
+        estrattoDue = new Semaphore(0);
+        estrattoUno = new Semaphore(0);
+    }
+
+    public synchronized Semaphore getEstrattoUno() {
+        return estrattoUno;
+    }
+
+    public synchronized Semaphore getEstrattoDue() {
+        return estrattoDue;
+    }
+
+    public synchronized int getPunatatoUno() {
+        return punatatoUno;
+    }
+
+    public synchronized int getPuntatoDue() {
+        return puntatoDue;
+    }
+
+    public synchronized Vector<Ruota> getRuote() {
+        return ruote;
+    }
 }
--- eseLotto/src/ThEstrai.java
+++ eseLotto/src/ThEstrai.java
@@ -1,2 +1,21 @@
-public class ThEstrai {
+import java.util.Random;
+
+public class ThEstrai extends Thread{
+    private final DatiCondivisi ptrDati;
+
+
+    public ThEstrai(DatiCondivisi ptrDati) {
+        this.ptrDati = ptrDati;
+    }
+
+    @Override
+    public void run() {
+        Random rn = new Random();
+        for (Ruota r : ptrDati.getRuote()) {
+            for(int i =0;i<Ruota.getNumeriPerRuota();i++)
+                r.setAt(i,rn.nextInt(100));
+            ptrDati.getEstrattoUno().release();
+            ptrDati.getEstrattoDue().release();
+        }
+    }
 }
