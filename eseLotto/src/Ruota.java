ea2d28224054ff0db8f2f9074f4f00f09b0cb137
jack96489 <giacomo.orsenigo@gmail.com>
2019-03-20 15:00:34 +0100

    Ruota

---
+++ eseLotto/src/Ruota.java
@@ -0,0 +1,50 @@
+public class Ruota {
+    private static final int NUMERI_PER_RUOTA = 5;
+
+    private final int numRuota;
+    private int[] estratti;
+    private boolean vintoUno, vintoDue;
+
+    public Ruota(int numRuota) {
+        this.numRuota = numRuota;
+        estratti = new int[NUMERI_PER_RUOTA];
+        vintoUno = false;
+        vintoDue = false;
+    }
+
+    public synchronized static int getNumeriPerRuota() {
+        return NUMERI_PER_RUOTA;
+    }
+
+    public synchronized int getNumRuota() {
+        return numRuota;
+    }
+
+    public synchronized void setAt(int index, int val) {
+        estratti[index] = val;
+    }
+
+    public synchronized int getAt(int index) {
+        return estratti[index];
+    }
+
+    public synchronized boolean isVintoUno() {
+        return vintoUno;
+    }
+
+    public synchronized void setVintoUno(boolean vintoUno) {
+        this.vintoUno = vintoUno;
+    }
+
+    public synchronized boolean isVintoDue() {
+        return vintoDue;
+    }
+
+    public synchronized void setVintoDue(boolean vintoDue) {
+        this.vintoDue = vintoDue;
+    }
+
+    public synchronized boolean hasWon() {
+        return vintoUno && vintoDue;
+    }
+}
